cc3e97a04bb770fe847ddd5b9df4efae7b060738
WJ Welch <wjwelchusa@gmail.com>
2022-01-11 17:06:47 -0600

    Java tutorial wrt conversions between string, ints.

---
+++ Prac4/src/Prac4.java
@@ -0,0 +1,30 @@
+ 
+
+// Reference resource Caleb uses is Wiki Books - Java Programming
+//  https://en.wikibooks.org/wiki/Java_Programming
+public class Prac4 {
+
+	public static void main(String[] args) {
+		
+		String numA = "23";
+		String numB = "47";
+		int i_numA;
+		int i_numB;
+		int result;
+		
+		i_numA = Integer.parseInt(numA);
+		i_numB = Integer.parseInt(numB);
+		
+		result = i_numA + i_numB;
+		System.out.println(result);
+		
+		
+
+
+
+
+
+	}
+	 
+
+}
