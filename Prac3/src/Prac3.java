b2f7c4dd3d64b8ebd171ffee1175f5706c7b7de9
WJ Welch <wjwelchusa@gmail.com>
2022-01-11 16:54:31 -0600

    Practiced operations casting from String to Integer and String to int data types.

--- Prac3/src/Prac3.java
+++ Prac3/src/Prac3.java
@@ -6,9 +6,17 @@ public class Prac3 {
 
 	public static void main(String[] args) {
 		
-		int x = 5;
-		System.out.println(++x);
-
+		String numA = "23";
+		String numB = "47";
+		int i_numA;
+		int i_numB;
+		int result;
+		
+		i_numA = Integer.parseInt(numA);
+		i_numB = Integer.parseInt(numB);
+		
+		result = i_numA + i_numB;
+		System.out.println(result);
 		
 		
 
